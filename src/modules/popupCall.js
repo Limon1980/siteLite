bc2c0a7a1fee9be511e7278ed38f1d7bac8855d4
Limon1980 <tarkalmob@yandex.ru>
2020-09-23 23:26:47 +1000

    модальное окно 1 закрывается на подложку и крестик

--- src/modules/popupCall.js
+++ src/modules/popupCall.js
@@ -6,15 +6,19 @@ const popupCall = () => {
 
 	window.addEventListener('click', e => {
 		const target = e.target;
+		if (popup.style.display === 'block' && target.closest('.popup-call') === target) {
+			popup.style.display = 'none';
+		}
 		contacts.forEach(item => {
 			if (target.closest('.contacts') === item) {
 				popup.style.display = 'block';
-			} else if (target.closest('.popup-close') === popupClose) {
-				e.preventDefault();
-				popup.style.display = 'none';
 			}
-
 		});
+		if (target.closest('.popup-close') === popupClose) {
+			e.preventDefault();
+			popup.style.display = 'none';
+		}
+
 	});
 
 };
