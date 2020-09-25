79dfb9c5aab181fddb89a77ce674600267e01afa
Limon1980 <tarkalmob@yandex.ru>
2020-09-25 19:16:17 +1000

    sentenceBtn add по кнопке Больше

--- src/modules/popupDiscount.js
+++ src/modules/popupDiscount.js
@@ -2,7 +2,8 @@ const popupDiscount = () => {
 
 	const popup = document.querySelector('.popup-discount');
 	const sentenceBtn = document.querySelectorAll('.sentence-btn');
-
+	const addSentenceBtn = document.querySelector('.add-sentence-btn');
+	const shadowBlock = document.querySelectorAll('.shadow-block');
 
 	sentenceBtn.forEach(item => {
 		item.addEventListener('click', e => {
@@ -15,6 +16,24 @@ const popupDiscount = () => {
 		});
 
 	});
+
+
+	addSentenceBtn.addEventListener('click', () => {
+
+		shadowBlock.forEach((item, key) => {
+			if (key > 2) {
+				item.parentNode.style.cssText = `
+				display: block!important
+			`;
+				addSentenceBtn.style.display = 'none';
+			}
+		});
+	});
+
+
+
+
+
 
 };
 
